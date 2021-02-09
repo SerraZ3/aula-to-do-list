3f41b8c3b93a8ef862df3c9e5f570de0edcda10d
SerraZ3 <serra.henrique3@gmail.com>
2021-02-09 20:22:28 -0300

    Adicionando lista e removendo lista to do

--- js/main.js
+++ js/main.js
@@ -9,29 +9,10 @@ var listas = [
       },
     ],
   },
-  {
-    titulo: "Fazendo",
-    todos: [
-      {
-        titulo: "Arrumar a casa 2",
-        status: 1,
-        descricao: "Começar limpando a cozinha",
-      },
-    ],
-  },
-  {
-    titulo: "Feito",
-    todos: [
-      {
-        titulo: "Arrumar a casa 2",
-        status: 1,
-        descricao: "Começar limpando a cozinha",
-      },
-    ],
-  },
 ];
 
 var statusTodo = ["A fazer", "Fazendo", "Feito"];
+
 // console.log(listas);
 // var novoArray = listas.map((value, index) => {
 //   console.log("lista", value);
@@ -48,52 +29,88 @@ var statusTodo = ["A fazer", "Fazendo", "Feito"];
 function render() {
   let container = document.getElementsByClassName("to-do-container")[0];
   let novoConteudo = listas.map((lista, idxLista) => {
-    console.log(lista.titulo);
     let template =
-      '<div class="col-sm-12 col-md-6 col-lg-4 mx-2 shadow-sm d-block to-do-list">' +
-      '<div class="row mx-2">' +
-      '<div class="col-12 to-do-list-header">' +
-      '<div class="to-do-list-title text-center">' +
-      `<h4>${lista.titulo}</h4>` +
-      "</div>" +
-      '<div class="to-do-list-config to-do-icons">' +
-      '<div class="to-do-icon-btn remove-list">' +
-      '<i class="fas fa-minus align-middle"></i>' +
-      "</div>" +
-      '<div class="to-do-icon-btn add-to-do">' +
-      '<i class="fas fa-plus align-middle"></i>' +
-      "</div>" +
-      '<div class="to-do-icon-btn config-list">' +
-      '<i class="fas fa-cog align-middle"></i>' +
-      "</div>" +
-      "</div>" +
-      "</div>" +
-      '<div class="col-12 to-do-list-body">' +
-      '<div class="row to-do aligin-items-center shadow-sm">' +
-      '<div class="col-6">' +
-      '<div class="status-container">' +
-      '<p class="to-do-text">Status:' +
-      '<span class="status">Fazendo</span>' +
-      "</p>" +
-      "</div>" +
-      "</div>" +
-      '<div class="col-6 to-do-icons">' +
-      '<div class="to-do-icon-btn config-to-do">' +
-      '<i class="fas fa-cog aligin-middle"></i>' +
-      "</div>" +
-      '<div class="to-do-icon-btn remove-to-do">' +
-      '<i class="fas fa-minus aligin-middle"></i>' +
-      "</div>" +
-      "</div>" +
-      '<div class="col-12">' +
-      '<p class="to-do-text">Limpar a casa</p>' +
-      "</div>" +
-      "</div>" +
-      "</div>" +
-      "</div>" +
-      "</div>";
+      `<div class="col-sm-12 col-md-6 col-lg-4 mx-2 shadow-sm d-block to-do-list" data-key="${idxLista}">` +
+      ' <div class="row mx-2">' +
+      '   <div class="col-12 to-do-list-header">' +
+      '     <div class="to-do-list-title text-center">' +
+      `       <h4>${lista.titulo}</h4>` +
+      "     </div>" +
+      '     <div class="to-do-list-config to-do-icons">' +
+      `       <div class="to-do-icon-btn remove-list" data-key="${idxLista}">` +
+      '         <i class="fas fa-minus align-middle"></i>' +
+      "       </div>" +
+      '       <div class="to-do-icon-btn add-to-do">' +
+      '         <i class="fas fa-plus align-middle"></i>' +
+      "       </div>" +
+      '       <div class="to-do-icon-btn config-list">' +
+      '         <i class="fas fa-cog align-middle"></i>' +
+      "       </div>" +
+      "     </div>" +
+      "   </div>" +
+      ' <div class="col-12 to-do-list-body">';
+    let todoHTML = lista.todos.map((todo, idxTodo) => {
+      let todoAux = statusTodo[todo.status];
+      return (
+        '<div class="row to-do aligin-items-center shadow-sm">' +
+        '<div class="col-6">' +
+        '<div class="status-container">' +
+        '<p class="to-do-text">Status:' +
+        `<span class="status">${todoAux}</span>` +
+        "</p>" +
+        "</div>" +
+        "</div>" +
+        '<div class="col-6 to-do-icons">' +
+        '<div class="to-do-icon-btn config-to-do">' +
+        '<i class="fas fa-cog aligin-middle"></i>' +
+        "</div>" +
+        '<div class="to-do-icon-btn remove-to-do">' +
+        '<i class="fas fa-minus aligin-middle"></i>' +
+        "</div>" +
+        "</div>" +
+        '<div class="col-12">' +
+        `<p class="to-do-text">${todo.titulo}</p>` +
+        "</div>" +
+        "</div>"
+      );
+    });
+    template += todoHTML.join("");
+    template += "</div>" + "</div>" + "</div>";
     return template;
   });
   container.innerHTML = novoConteudo.join("");
 }
-render();
+
+function addTodoList() {
+  let titleInput = document.getElementById("title-list-name");
+
+  listas.push({
+    titulo: titleInput.value,
+    todos: [],
+  });
+  titleInput.value = "";
+  resetContainer();
+}
+
+function removeTodoList() {
+  delete listas[this.dataset.key];
+
+  resetContainer();
+}
+function addEvents() {
+  let removeLists = document.getElementsByClassName("remove-list");
+
+  for (var i = 0; i < removeLists.length; i++) {
+    removeLists[i].addEventListener("click", removeTodoList);
+  }
+}
+function resetContainer() {
+  render();
+  addEvents();
+}
+
+document
+  .getElementById("add-to-do-list")
+  .addEventListener("click", addTodoList);
+
+resetContainer();
